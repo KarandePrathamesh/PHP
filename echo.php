24a173bd9611350334eb7dd3ca77bf4aadf5402d
KarandePrathamesh <karandep703@gmail.com>
2025-07-04 22:46:19 +0530

    echo in php language constructor

---
+++ echo.php
@@ -0,0 +1,13 @@
+<?php
+echo 10+20;
+echo"Prathamesh Karande";
+echo"</br>";
+echo"age: 21";
+echo"</br>";
+echo"Village:Sonawadi";
+echo"</br>";
+echo"City:Satara";
+
+echo(" echo as a function still it is a language constructor");
+?>
+<!-- echo 10+20;  this will print as it is without calculation -->
