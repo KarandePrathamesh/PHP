0a14051155e02e58eadf868342a03e260cf35f13
KarandePrathamesh <karandep703@gmail.com>
2025-07-08 16:40:10 +0530

    display errors in php

---
+++ display-error.php
@@ -0,0 +1,12 @@
+<!-- Display Errors in PHP:
+    1. why do we need to display errors?
+    2. check the php.ini file location
+    3. change display error property 
+    4. test errors.
+ -->
+<?PHP
+    echo phpversion()
+
+    echo phpinfo();  // getting php.ini file path an setting it's display errors true in order to see error on localhost pages
+    
+?>
